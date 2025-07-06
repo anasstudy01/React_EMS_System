f10b487d0bc82df0f003dd5fb8f33600ab4416d6
anasstudy01 <anasstudy01@gmail.com>
2025-07-07 01:57:54 +0530

    feat: initialize React project with Vite and Tailwind CSS
    - Added package.json with dependencies for React, Vite, Tailwind CSS, and ESLint.
    - Created initial SVG assets for Vite and React logos.
    - Set up global CSS with Tailwind CSS imports.
    - Developed main App component structure with placeholder for Login component.
    - Implemented Login component with form handling for email and password.
    - Created basic Auth component and context for authentication.
    - Added components for task management: AcceptTask, CompleteTask, FailedTask, NewTask.
    - Established context for task management.
    - Configured Vite for React and Tailwind CSS integration.

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // Function to handle form submission
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("Form submitted");
+    console.log(`Email: ${email}, Password: ${password}`);
+
+    setEmail("");
+    setPassword("");
+  };
+
+
+
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="border border-green-600 rounded-lg p-8 w-80 mx-auto mt-20">
+      <form  onSubmit={submitHandler} className="flex flex-col">
+
+        <input
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Enter your email"
+          className="w-full mb-4 px-4 py-2 border border-green-600 rounded-full bg-transparent text-white placeholder-white focus:outline-none"
+        />
+        <input
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="Enter password"
+          className="w-full mb-6 px-4 py-2 border border-green-600 rounded-full bg-transparent text-white placeholder-white focus:outline-none"
+        />
+        <button required  className="bg-green-600 text-white px-6 py-2 rounded-full hover:bg-green-700 w-full">
+          Log in
+          
+        </button>
+      </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
