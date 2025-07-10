88b5c482a402e2db78a745216cb2fe79fef61c3e
anas <anasstudy01@gmail.com>
2025-07-10 15:41:26 +0530

    feat: enhance login handling and improve user role management

--- src/App.jsx
+++ src/App.jsx
@@ -9,29 +9,48 @@ import { AuthContext } from "./context/AuthProvider";
 const App = () => {
   const [user, setUser] = useState(null);
   const authData = useContext(AuthContext);
-  console.log("Auth Data:", authData);
-  
+  const [loggedInUserData, setLoggedInUserData] = useState(null);
 
+  // console.log("Auth Data:", authData);
 
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem("loggedInUser");
+    if (loggedInUser) {
+      setUser(JSON.parse(loggedInUser));
+    }
+  }, []);
 
+  //   useEffect(()=>{
+  // if(authData){
+  //   const loggedInUser =localStorage.getItem("loggedInUser");
+  //   if(loggedInUser){
+  //     setUser(loggedInUser.role);
+  //   }
+  // }
+
+  //   },[authData])
 
   const handleLogin = (email, password) => {
     if (email == "admin@me.com" && password == "123") {
-      setUser("admin");
-    } else if (email == "employee@me.com" && password == "123") {
-      setUser("employee");
-    } else {
-      alert("Invalid credentials");
+      setUser({ role: "admin" });
+      localStorage.setItem("loggedInUser", JSON.stringify({ role: "admin" }));
+    } else if (authData) {
+      const employee = authData.employees.find((e) => email == e.email && password == e.password);
+      if (employee) {
+        setUser({ role: "employee" });
+        setLoggedInUserData(employee);
+        localStorage.setItem("loggedInUser", JSON.stringify({ role: "employee" }));
+      } else {
+        alert("Invalid credentials");
+      }
     }
   };
 
   return (
     <>
-      <div>
-        {!user ? <Login handleLogin={handleLogin} /> : ""}
-        {user && user.role === "employee" && <EmployeeDashbord />}
-        {user && user.role === "admin" && <AdminDashbord />}
-      </div>
+      {!user ? <Login handleLogin={handleLogin} /> : ""}
+      {user?.role == 'admin' ?  <AdminDashbord /> :<EmployeeDashbord/>}
+     
     </>
   );
 };
